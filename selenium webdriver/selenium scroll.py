75302274d97e26539096f7c6406be35f8b3c1bd3
Isha <136315667+IshaAppPerfect@users.noreply.github.com>
2023-06-19 14:50:45 +0530

    Update selenium scroll.py
    adding comment at 8th line

--- selenium webdriver/selenium scroll.py
+++ selenium webdriver/selenium scroll.py
@@ -5,6 +5,7 @@ driver=webdriver.Chrome()
 driver.get("https://en.wikipedia.org")
 
 driver.maximize_window()
+#two methods of scrolling-----
 #scroll by pixel
 driver.execute_script("window.scrollBy(0,2000)","")
 #driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
